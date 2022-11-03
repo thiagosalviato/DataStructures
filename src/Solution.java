d3ec8fc136818f97cb8fa1cdf6833953b68b28d8
Thiago Salviato <thiago_salviato@outlook.com>
2022-11-03 18:43:37 -0300

    Add String Manipulation problems

--- src/Solution.java
+++ src/Solution.java
@@ -90,5 +90,17 @@ public class Solution {
         return count;
     }
 
+    public void reverseString(char[] s) {
+        int a_pointer = 0;
+        int b_pointer = s.length-1;
 
+        while (a_pointer <= b_pointer){
+            char temp = s[a_pointer];
+            s[a_pointer] = s[b_pointer];
+            s[b_pointer] = temp;
+
+            a_pointer += 1;
+            b_pointer -= 1;
+        }
+    }
 }
